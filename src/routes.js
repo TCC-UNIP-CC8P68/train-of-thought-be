f8ede0b28c580ddb9e166d11e8c86713e7949bcc
MatheusTurra <matheusfr.318@gmail.com>
2021-08-26 21:35:08 -0300

    Send timeout value to api

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
-const controller = require('./controllers/captures');
+const controllerCapture = require('./controllers/captures');
+const controllerConfiguration = require('./controllers/configurations');
 
 module.exports = (app) => {
   app.route('/capture')
-  .post(controller.postCapture)
-  .get(controller.getCapture);
+  .post(controllerCapture.postCapture)
+  .get(controllerCapture.getCapture);
+
+  app.route('/configuration')
+  .post(controllerConfiguration.postConfiguration);
 }
